d4734e9b1c4f99c0534af938adc0a8c27c090895
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-03-18 16:08:26 -0700

    added click response for open placement locations

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 angular.module('App', [])
-    .controller('Game', function(){
+    .controller('Game', ['$log', function($log){
 
         var n = 8;
         var board = [];
         for (let i = 0; i < n; i++){
             let line = [];
             for (let j = 0; j < n; j++) {
-                line.push({classes:'', hover: false})
+                line.push({white: false, black: false, option: false, hover: false});
             }
             board.push(line);
         }
 
-        board[0][5].classes = 'p w';
-        board[5][0].classes = 'p b';
-        board[4][4].classes = 'o w p';
-        angular.extend(this, {board: board});
+        var methods = {};
 
-        //console.log(this.board);
-    });
+        methods.debug = $log;
+
+        methods.click = function(row, column, space) {
+            if(space.option){
+                this.debug.log(row, column);
+            }
+        }
+        
+        
+        board[3][3].white = true;
+        board[4][4].white = true;
+        board[3][4].black = true;
+        board[4][3].black = true;
+
+        board[2][4].white = true;
+        board[2][4].option = true;
+
+
+        angular.extend(this, {board: board}, methods);
+    }]);
